f3054bb305c03ce107498556e2171f71a2e0b0bd
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-22 15:40:31 +0700

    update TextArea.js docs

--- src/components/input/TextArea.js
+++ src/components/input/TextArea.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 
+/*TODO: Phần tử textarea trong React hơi khác so với HTML thông thường.
+    Trong HTML, giá trị của một textarea là văn bản giữa thẻ bắt đầu <textarea> và thẻ kết thúc </textarea>*/
 function Form() {
     const [state, setState] = useState({
         description: "The content of a textarea goes in the value attribute"
